72ef37993a01b8325209935a715c37745a4b28f9
Julien Philippe <jujup45300@gmail.com>
2026-09-16 12:16:44 +0200

    green: calculer le nombre de demi-heures commencées au-delà de 30 min

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,3 +1,8 @@
 def calculer_prix(duree_minutes, abonne=False, electrique=False):
+    prix = 0
     if duree_minutes <= 30:
-        return 0.00
+        prix = 0.00
+    else:
+        nb_heures = (duree_minutes - 30 + 29) // 30
+        prix = 1.5 * nb_heures
+    return prix
