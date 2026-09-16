81b84c4dd56905ad1df4eab3b4b6b6b8e87bab5f
Julien Philippe <jujup45300@gmail.com>
2026-09-16 14:12:57 +0200

    green: plafonner le montant à 18 euros par tranche de 24h

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,8 +1,16 @@
+import math
+
+
 def calculer_prix(duree_minutes, abonne=False, electrique=False):
     prix = 0
     if duree_minutes <= 30:
         prix = 0.00
     else:
-        nb_heures = (duree_minutes - 30 + 29) // 30
-        prix = 1.5 * nb_heures
+        nb_demi_heures = math.ceil((duree_minutes - 30) / 30)
+        prix = 1.5 * nb_demi_heures
+
+    nb_tranches_24h = math.ceil(duree_minutes / 1440)
+    plafond = 18.0 * nb_tranches_24h
+    prix = min(prix, plafond)
+
     return prix
