19da3e241e6eafa0c2b3d6cc9598d5b5d2a5e1f3
Julien Philippe <jujup45300@gmail.com>
2026-09-17 10:30:24 +0200

    test : fonction reuperer_article_enalerte fonctionnel

--- tp1-philippe/inventaire/test_inventaire.py
+++ tp1-philippe/inventaire/test_inventaire.py
@@ -1,4 +1,4 @@
-from inventaire import calculer_valeur_stock
+from inventaire import calculer_valeur_stock, recuperer_article_en_alerte
 
 
 def test_calculer_valeur_stock_standard():
@@ -8,3 +8,10 @@ def test_calculer_valeur_stock_standard():
     ]
     assert calculer_valeur_stock(articles) == 70.0
 
+def test_recuperer_article_en_alerte():
+    articles = [
+        {"ref": "A1", "q": 1, "seuil": 2},
+        {"ref": "A2", "q": 2, "seuil": 2},
+        {"ref": "A3", "q": 5, "seuil": 2},
+    ]
+    assert recuperer_article_en_alerte(articles) == ["A1"]
