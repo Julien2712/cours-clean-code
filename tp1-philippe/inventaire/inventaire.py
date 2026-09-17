7907f0b055d80145c9ee6e0debb2128bd1d4dd42
Julien Philippe <jujup45300@gmail.com>
2026-09-17 16:08:13 +0200

    test: et refactor de toutes les fonctions

--- tp1-philippe/inventaire/inventaire.py
+++ tp1-philippe/inventaire/inventaire.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
-# gestion de stock entrepot nord - v4
-# repris de la v3 de Kevin, TODO refactorer un jour
-# NE PAS TOUCHER A mouv() SANS PREVENIR L'EQUIPE LOGISTIQUE
 import datetime
 import json
 import math
-import random
 
 TVA = 0.2
 FACTEUR_DE_REAPPROVISIONNEMENT = 3
 REMISE_REAPPROVISIONNEMENT = 0.1
 QUANTITE_REAPPROVISIONNEMENT = 100
+JOURS_PERIODE_VENTES = 30
+MOUVEMENT_SORTIE = "out"
+MOUVEMENT_ENTREE = "in"
+CATEGORIES_CONNUES = ("outil", "consommable", "piece")
+
 JOURNAL = []
-STOCK = {}
 DERNIER = 0
 
 
@@ -32,10 +31,6 @@ def recuperer_article_en_alerte(articles):
     return reference_alerte
 
 
-MOUVEMENT_SORTIE = "out"
-MOUVEMENT_ENTREE = "in"
-
-
 def enregistrer_mouvement_stock(article, quantite, type_mouvement=MOUVEMENT_SORTIE, forcer=False):
     global DERNIER
 
@@ -52,12 +47,14 @@ def enregistrer_mouvement_stock(article, quantite, type_mouvement=MOUVEMENT_SORT
         return False
 
     DERNIER += 1
-    JOURNAL.append({
-        "id": DERNIER,
-        "ref": article["ref"],
-        "q": quantite,
-        "t": type_mouvement,
-    })
+    JOURNAL.append(
+        {
+            "id": DERNIER,
+            "ref": article["ref"],
+            "q": quantite,
+            "t": type_mouvement,
+        }
+    )
     return True
 
 
@@ -65,9 +62,7 @@ def calculer_cout_reapprovisionnement(article):
     if article["q"] >= article["seuil"]:
         return 0
 
-    quantite_a_commander = (
-        article["seuil"] * FACTEUR_DE_REAPPROVISIONNEMENT - article["q"]
-    )
+    quantite_a_commander = article["seuil"] * FACTEUR_DE_REAPPROVISIONNEMENT - article["q"]
     cout_total = quantite_a_commander * article["pu"]
 
     if quantite_a_commander > QUANTITE_REAPPROVISIONNEMENT:
@@ -75,124 +70,70 @@ def calculer_cout_reapprovisionnement(article):
 
     return round(cout_total, 2)
 
-cout = calculer_cout_reapprovisionnement
+
+def trier_articles_par_valeur(articles):
+    return sorted(
+        articles,
+        key=lambda article: article["q"] * article["pu"],
+        reverse=True,
+    )
 
 
-
-def classer(arts):
-    l = []
-    for a in arts:
-        l.append(a)
-    for i in range(len(l)):
-        for k in range(len(l) - 1):
-            if l[k]["q"] * l[k]["pu"] < l[k + 1]["q"] * l[k + 1]["pu"]:
-                tmp = l[k]
-                l[k] = l[k + 1]
-                l[k + 1] = tmp
-    return l
-
-
-def rot(a, v):
+def calculer_rotation_stock(article, ventes_periode):
     try:
-        return math.floor(a["q"] / (v / 30))
-    except:
+        return math.floor(article["q"] / (ventes_periode / JOURS_PERIODE_VENTES))
+    except ZeroDivisionError:
         return 0
 
 
-def par_cat(arts):
-    d = {}
-    for a in arts:
-        if a["cat"] == "outil":
-            if "outil" in d:
-                d["outil"] = d["outil"] + a["q"] * a["pu"]
-            else:
-                d["outil"] = a["q"] * a["pu"]
-        elif a["cat"] == "consommable":
-            if "consommable" in d:
-                d["consommable"] = d["consommable"] + a["q"] * a["pu"]
-            else:
-                d["consommable"] = a["q"] * a["pu"]
-        elif a["cat"] == "piece":
-            if "piece" in d:
-                d["piece"] = d["piece"] + a["q"] * a["pu"]
-            else:
-                d["piece"] = a["q"] * a["pu"]
-        else:
-            if "autre" in d:
-                d["autre"] = d["autre"] + a["q"] * a["pu"]
-            else:
-                d["autre"] = a["q"] * a["pu"]
-    for k in d:
-        d[k] = round(d[k], 2)
-    return d
+def regrouper_valeur_par_categorie(articles):
+    valeurs = {}
+    for article in articles:
+        categorie = article["cat"] if article["cat"] in CATEGORIES_CONNUES else "autre"
+        valeurs[categorie] = valeurs.get(categorie, 0) + article["q"] * article["pu"]
+    return {categorie: round(valeur, 2) for categorie, valeur in valeurs.items()}
 
 
-def rapport(arts, ventes=None, cat=None, seuil_min=None, export=False, verbose=True, d=None):
-    if d is None:
-        d = datetime.datetime.now()
-    res = {}
-    res["date"] = str(d)
-    tot = 0
-    nb = 0
-    liste_alerte = []
-    for a in arts:
-        if cat is not None:
-            if a["cat"] != cat:
-                continue
-        if seuil_min is not None:
-            if a["q"] < seuil_min:
-                continue
-        if a["q"] > 0:
-            if a["pu"] > 0:
-                tot = tot + a["q"] * a["pu"]
-                nb = nb + 1
-                if a["q"] < a["seuil"]:
-                    liste_alerte.append(a["ref"])
-                    if verbose:
-                        print("ALERTE " + a["ref"] + " : " + str(a["q"]) + " restants")
-                if ventes is not None:
-                    if a["ref"] in ventes:
-                        if ventes[a["ref"]] > 0:
-                            j = math.floor(a["q"] / (ventes[a["ref"]] / 30))
-                            if j < 7:
-                                if verbose:
-                                    print("RUPTURE IMMINENTE " + a["ref"])
-                            elif j < 30:
-                                if verbose:
-                                    print("a surveiller " + a["ref"])
-                        else:
-                            if verbose:
-                                print("aucune vente pour " + a["ref"])
-            else:
-                if verbose:
-                    print("prix invalide " + a["ref"])
-        else:
-            if verbose:
-                print("stock vide " + a["ref"])
-    res["valeur"] = round(tot, 2)
-    res["nb"] = nb
-    res["alertes"] = liste_alerte
-    res["ttc"] = round(tot * (1 + TVA), 2)
-    if export:
-        f = open("/tmp/rapport_" + str(random.randint(1, 9999)) + ".json", "w")
-        f.write(json.dumps(res))
-        f.close()
-    return res
+def filtrer_articles_valides(articles, cat=None, seuil_min=None):
+    return [
+        article
+        for article in articles
+        if (cat is None or article["cat"] == cat)
+        and (seuil_min is None or article["q"] >= seuil_min)
+        and article["q"] > 0
+        and article["pu"] > 0
+    ]
 
 
-def maj_prix(ref, p):
-    # ancienne version, remplacee par l'ERP en 2021
-    # for a in STOCK:
-    #     if a == ref:
-    #         STOCK[a]["pu"] = p
-    #         JOURNAL.append({"ref": ref, "p": p})
-    # return True
-    return None
+def calculer_synthese_stock(articles):
+    valeur_totale = round(sum(article["q"] * article["pu"] for article in articles), 2)
+    alertes = [article["ref"] for article in articles if article["q"] < article["seuil"]]
+    return {
+        "valeur": valeur_totale,
+        "nb": len(articles),
+        "alertes": alertes,
+        "ttc": round(valeur_totale * (1 + TVA), 2),
+    }
 
 
-def export_json(res, chemin="/tmp/inv.json", hist=[]):
+def afficher_alertes(alertes):
+    for reference in alertes:
+        print(f"ALERTE {reference}")
+
+
+def rapport(articles, cat=None, seuil_min=None, verbose=True):
+    articles_valides = filtrer_articles_valides(articles, cat, seuil_min)
+    synthese = calculer_synthese_stock(articles_valides)
+    synthese["date"] = str(datetime.datetime.now())
+    if verbose:
+        afficher_alertes(synthese["alertes"])
+    return synthese
+
+
+def export_json(res, chemin="/tmp/inv.json", hist=None):
+    if hist is None:
+        hist = []
     hist.append(res)
-    f = open(chemin, "w")
-    f.write(json.dumps(hist))
-    f.close()
+    with open(chemin, "w", encoding="utf-8") as fichier:
+        fichier.write(json.dumps(hist))
     return hist
--- tp1-philippe/inventaire/test_inventaire.py
+++ tp1-philippe/inventaire/test_inventaire.py
@@ -1,31 +1,248 @@
-from inventaire import MOUVEMENT_SORTIE,calculer_cout_reapprovisionnement ,calculer_valeur_stock, recuperer_article_en_alerte, enregistrer_mouvement_stock
+import inventaire
+from inventaire import (
+    MOUVEMENT_ENTREE,
+    MOUVEMENT_SORTIE,
+    calculer_cout_reapprovisionnement,
+    calculer_rotation_stock,
+    calculer_synthese_stock,
+    calculer_valeur_stock,
+    enregistrer_mouvement_stock,
+    export_json,
+    filtrer_articles_valides,
+    rapport,
+    recuperer_article_en_alerte,
+    regrouper_valeur_par_categorie,
+    trier_articles_par_valeur,
+)
 
 
-def test_calculer_valeur_stock_standard():
+def test_valeur_stock_standard():
     articles = [
-        {"ref": "A1", "q": 10, "pu": 5.0, "seuil": 2},
-        {"ref": "A2", "q": 2, "pu": 10.0, "seuil": 5},
+        {"ref": "A1", "q": 10, "pu": 5.0},
+        {"ref": "A2", "q": 2, "pu": 10.0},
     ]
     assert calculer_valeur_stock(articles) == 70.0
 
-def test_recuperer_article_en_alerte():
+
+def test_valeur_stock_ignore_les_quantites_negatives_ou_nulles():
     articles = [
-        {"ref": "A1", "q": 1, "seuil": 2},
-        {"ref": "A2", "q": 2, "seuil": 2},
-        {"ref": "A3", "q": 5, "seuil": 2},
+        {"ref": "A1", "q": -5, "pu": 10.0},
+        {"ref": "A2", "q": 0, "pu": 10.0},
+        {"ref": "A3", "q": 3, "pu": 10.0},
     ]
+    assert calculer_valeur_stock(articles) == 30.0
+
+
+def test_valeur_stock_est_arrondie_au_centime():
+    articles = [{"ref": "A1", "q": 3, "pu": 0.333}]
+    assert calculer_valeur_stock(articles) == 1.0
+
+
+def test_valeur_stock_liste_vide():
+    assert calculer_valeur_stock([]) == 0
+
+
+def test_article_strictement_sous_le_seuil_est_en_alerte():
+    articles = [{"ref": "A1", "q": 1, "seuil": 2}]
     assert recuperer_article_en_alerte(articles) == ["A1"]
 
-def test_enregistrer_mouvement_stock():
-    articles = [{"ref": "A1", "q": 10, "seuil": 2}]
-    assert enregistrer_mouvement_stock(articles[0], 5) == True
-    assert enregistrer_mouvement_stock(articles[0], -2) == False
-    assert enregistrer_mouvement_stock(articles[0], 15, MOUVEMENT_SORTIE, forcer=True) == True
 
-def test_calculer_cout_reapprovisionnement():
-    art_ok = {"q": 10, "seuil": 5, "pu": 2.0}
-    assert calculer_cout_reapprovisionnement(art_ok) == 0
-    art_alerte = {"q": 2, "seuil": 5, "pu": 2.0}
-    assert calculer_cout_reapprovisionnement(art_alerte) == 26.0
-    art_remise = {"q": 50, "seuil": 100, "pu": 10.0}
-    assert calculer_cout_reapprovisionnement(art_remise) == 2250.0
+def test_article_exactement_au_seuil_n_est_PAS_en_alerte_ECART_M2():
+    articles = [{"ref": "A1", "q": 2, "seuil": 2}]
+    assert recuperer_article_en_alerte(articles) == []
+
+
+def test_article_au_dessus_du_seuil_n_est_pas_en_alerte():
+    articles = [{"ref": "A1", "q": 5, "seuil": 2}]
+    assert recuperer_article_en_alerte(articles) == []
+
+
+def test_mouvement_sortie_valide_diminue_le_stock():
+    article = {"ref": "A1", "q": 10}
+    assert enregistrer_mouvement_stock(article, 5) is True
+    assert article["q"] == 5
+
+
+def test_mouvement_entree_augmente_le_stock():
+    article = {"ref": "A1", "q": 10}
+    assert enregistrer_mouvement_stock(article, 5, MOUVEMENT_ENTREE) is True
+    assert article["q"] == 15
+
+
+def test_mouvement_quantite_nulle_est_refuse_M4():
+    article = {"ref": "A1", "q": 10}
+    assert enregistrer_mouvement_stock(article, 0) is False
+    assert article["q"] == 10
+
+
+def test_mouvement_quantite_negative_est_refuse_M4():
+    article = {"ref": "A1", "q": 10}
+    assert enregistrer_mouvement_stock(article, -3) is False
+    assert article["q"] == 10
+
+
+def test_mouvement_sortie_superieure_au_stock_est_refuse():
+    article = {"ref": "A1", "q": 5}
+    assert enregistrer_mouvement_stock(article, 10) is False
+
+
+def test_mouvement_sortie_refusee_MODIFIE_QUAND_MEME_LE_STOCK_ECART_M3():
+    article = {"ref": "A1", "q": 5}
+    enregistrer_mouvement_stock(article, 10)
+    assert article["q"] == -5
+
+
+def test_mouvement_sortie_forcee_autorise_le_stock_negatif():
+    article = {"ref": "A1", "q": 5}
+    assert enregistrer_mouvement_stock(article, 10, MOUVEMENT_SORTIE, forcer=True) is True
+    assert article["q"] == -5
+
+
+def test_mouvement_type_inconnu_est_refuse():
+    article = {"ref": "A1", "q": 10}
+    assert enregistrer_mouvement_stock(article, 5, "transfert") is False
+
+
+def test_mouvement_incremente_dernier_et_alimente_le_journal():
+    article = {"ref": "A1", "q": 10}
+    avant = inventaire.DERNIER
+    enregistrer_mouvement_stock(article, 3)
+    assert inventaire.DERNIER == avant + 1
+    assert inventaire.JOURNAL[-1]["ref"] == "A1"
+    assert inventaire.JOURNAL[-1]["q"] == 3
+
+
+def test_cout_article_pas_en_alerte_est_nul():
+    article = {"q": 10, "seuil": 5, "pu": 2.0}
+    assert calculer_cout_reapprovisionnement(article) == 0
+
+
+def test_cout_article_exactement_au_seuil_est_nul():
+    article = {"q": 5, "seuil": 5, "pu": 2.0}
+    assert calculer_cout_reapprovisionnement(article) == 0
+
+
+def test_cout_article_en_alerte_sans_remise():
+    article = {"q": 2, "seuil": 5, "pu": 2.0}
+    assert calculer_cout_reapprovisionnement(article) == 26.0
+
+
+def test_cout_article_en_alerte_avec_remise_au_dela_de_100():
+    article = {"q": 50, "seuil": 100, "pu": 10.0}
+    assert calculer_cout_reapprovisionnement(article) == 2250.0
+
+
+def test_cout_a_exactement_100_unites_N_A_PAS_la_remise_ECART_M5():
+    article = {"q": 5, "seuil": 35, "pu": 10.0}
+    assert calculer_cout_reapprovisionnement(article) == 1000.0
+
+
+def test_tri_par_valeur_decroissante():
+    articles = [
+        {"ref": "A1", "q": 2, "pu": 10.0},
+        {"ref": "A2", "q": 10, "pu": 5.0},
+        {"ref": "A3", "q": 1, "pu": 5.0},
+    ]
+    resultat = trier_articles_par_valeur(articles)
+    assert [a["ref"] for a in resultat] == ["A2", "A1", "A3"]
+
+
+def test_tri_ne_modifie_pas_la_liste_d_origine():
+    articles = [{"ref": "A1", "q": 1, "pu": 1.0}, {"ref": "A2", "q": 2, "pu": 1.0}]
+    ordre_avant = [a["ref"] for a in articles]
+    trier_articles_par_valeur(articles)
+    assert [a["ref"] for a in articles] == ordre_avant
+
+
+def test_tri_liste_vide():
+    assert trier_articles_par_valeur([]) == []
+
+
+def test_rotation_calcul_standard():
+    article = {"q": 60}
+    assert calculer_rotation_stock(article, 30) == 60
+
+
+def test_rotation_aucune_vente_NE_LEVE_PAS_D_ERREUR_ECART_M7():
+    article = {"q": 60}
+    assert calculer_rotation_stock(article, 0) == 0
+
+
+def test_filtrer_articles_valides_exclut_quantite_et_prix_nuls():
+    articles = [
+        {"ref": "A1", "q": 0, "pu": 5.0, "seuil": 2, "cat": "outil"},
+        {"ref": "A2", "q": 5, "pu": 0, "seuil": 2, "cat": "outil"},
+        {"ref": "A3", "q": 5, "pu": 5.0, "seuil": 2, "cat": "outil"},
+    ]
+    resultat = filtrer_articles_valides(articles)
+    assert [a["ref"] for a in resultat] == ["A3"]
+
+
+def test_calculer_synthese_stock():
+    articles = [{"ref": "A1", "q": 10, "pu": 5.0, "seuil": 2}]
+    synthese = calculer_synthese_stock(articles)
+    assert synthese["valeur"] == 50.0
+    assert synthese["ttc"] == 60.0
+    assert synthese["nb"] == 1
+    assert synthese["alertes"] == []
+
+
+def test_rapport_calcule_la_valeur_et_le_ttc():
+    articles = [{"ref": "A1", "q": 10, "pu": 5.0, "seuil": 2, "cat": "outil"}]
+    res = rapport(articles, verbose=False)
+    assert res["valeur"] == 50.0
+    assert res["ttc"] == 60.0
+    assert res["nb"] == 1
+    assert res["alertes"] == []
+
+
+def test_rapport_signale_les_alertes():
+    articles = [{"ref": "A1", "q": 1, "pu": 5.0, "seuil": 2, "cat": "outil"}]
+    res = rapport(articles, verbose=False)
+    assert res["alertes"] == ["A1"]
+
+
+def test_rapport_filtre_par_categorie():
+    articles = [
+        {"ref": "A1", "q": 1, "pu": 5.0, "seuil": 2, "cat": "outil"},
+        {"ref": "A2", "q": 1, "pu": 5.0, "seuil": 2, "cat": "piece"},
+    ]
+    res = rapport(articles, cat="outil", verbose=False)
+    assert res["nb"] == 1
+
+
+def test_rapport_ECRIT_SUR_LA_CONSOLE_PAR_DEFAUT_ECART_M8(capsys):
+    articles = [{"ref": "A1", "q": 1, "pu": 5.0, "seuil": 2, "cat": "outil"}]
+    rapport(articles)
+    capture = capsys.readouterr()
+    assert "ALERTE A1" in capture.out
+
+
+def test_par_categorie_regroupe_les_valeurs():
+    articles = [
+        {"cat": "outil", "q": 2, "pu": 5.0},
+        {"cat": "outil", "q": 1, "pu": 5.0},
+        {"cat": "piece", "q": 1, "pu": 10.0},
+    ]
+    assert regrouper_valeur_par_categorie(articles) == {"outil": 15.0, "piece": 10.0}
+
+
+def test_par_categorie_inconnue_tombe_dans_autre():
+    articles = [{"cat": "gadget", "q": 1, "pu": 3.0}]
+    assert regrouper_valeur_par_categorie(articles) == {"autre": 3.0}
+
+
+def test_export_json_ecrit_le_fichier_et_retourne_l_historique(tmp_path):
+    chemin = tmp_path / "inv.json"
+    resultat = export_json({"valeur": 10}, chemin=str(chemin))
+    assert chemin.exists()
+    assert resultat == [{"valeur": 10}]
+
+
+def test_export_json_ne_partage_plus_l_historique_entre_appels_FIX_ARGUMENT_MUTABLE(tmp_path):
+    chemin_a = tmp_path / "inv_a.json"
+    chemin_b = tmp_path / "inv_b.json"
+    premier = export_json({"valeur": 1}, chemin=str(chemin_a))
+    deuxieme = export_json({"valeur": 2}, chemin=str(chemin_b))
+    assert premier is not deuxieme
+    assert len(deuxieme) == 1
